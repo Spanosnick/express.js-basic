21f4b4cfec5d16fcaf13afa4b7389114a4ebe21d
Spanos <nickspanosf1@gmail.com>
2024-11-27 19:23:03 +0200

    basic express install

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# express.js-basic" 
+1) npm init -y // create package.json
+2) npm install express // install express
+3) npm i --save-dev nodemon // install nodemon
+4) npm run devStart // to init Server
+5) npm install ejs // install ejs
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Set the routing
+app.get('/', (req, res) => {
+    // res.json({ message: 'Hello World!' });
+    // res.send('Hello World!');
+    // res.download('./server.js');
+    // res.render
+});
+
+
+app.listen(3000);
+
